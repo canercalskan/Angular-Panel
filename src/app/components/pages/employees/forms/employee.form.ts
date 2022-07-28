c2db1fa87f63693f75e5b52f36c6bce42d86e1f1
canercalskan <cvn3r8@gmail.com>
2022-07-28 13:54:47 +0300

    use single handler and form for employee actions

--- src/app/components/pages/employees/forms/employee.form.ts
+++ src/app/components/pages/employees/forms/employee.form.ts
@@ -21,6 +21,7 @@ export class EmployeeForm {
     if(this.router.url == '/Actions/Add') {
         this.addClicked = true;
         this.updateClicked = false;
+
     }
     else if(this.router.url == '/Actions/Update') {
         this.updateClicked = true;
@@ -40,56 +41,56 @@ export class EmployeeForm {
   newItemAdded: boolean = false;
   newItemFailed : boolean = false;
   newEmployeeID = Math.round(Math.random() * 100);
-  saveEmployee(employee: Employee): void {
-    this.updateClicked = false;
-    this.addClicked = true;
-    this.newItemAdded = true;
-    if (+employee.salary > 0) {
-      this.newItemFailed = false;
-      const newEmployee = {
-        id: (this.newEmployeeID++).toString(),
-        name: employee.name,
-        surname: employee.surname,
-        salary: employee.salary.toString(),
-        salaryUsd : employee.salaryUsd,
-      };
-      this.EmployeeService.addEmployees(newEmployee).subscribe(() => {
-        this.EmployeeService.getEmployees().subscribe();
-      });
-    }
-    else {
-      this.newItemAdded = false;
-      this.newItemFailed = true;
-      return;
-    }
-  }
 
-  updateEmployee(employee: Employee): void {
-    if (employee.name.length < 2) {
-      this.nameError = true;
-    } else {
-      this.nameError = false;
+  handleSubmit(employee:Employee) : void {
+    if(this.addClicked) {
+      this.newItemAdded = true;
+      if (+employee.salary > 0) {
+        this.newItemFailed = false;
+        const newEmployee = {
+          id: (this.newEmployeeID++).toString(),
+          name: employee.name,
+          surname: employee.surname,
+          salary: employee.salary.toString(),
+          salaryUsd : employee.salaryUsd,
+        };
+        this.EmployeeService.addEmployees(newEmployee).subscribe(() => {
+          this.EmployeeService.getEmployees().subscribe();
+        });
+      }
+      else {
+        this.newItemAdded = false;
+        this.newItemFailed = true;
+        return;
+      }
     }
-    if (employee.surname.length < 2) {
-      this.surnameError = true;
-    } else {
-      this.surnameError = false;
-    }
-    if (+employee.salary <= 0) {
-      this.salaryError = true;
-    } else {
-      this.salaryError = false;
-    }
-    if (!this.nameError && !this.surnameError && !this.salaryError) {
-      this.updatedEmployee.name = employee.name;
-      this.updatedEmployee.surname = employee.surname;
-      this.updatedEmployee.salary = employee.salary;
-      this.ConvertService.convertSalary(employee.salary.toString()).subscribe(response => {
-        this.updatedEmployee.salaryUsd = response.result.toFixed(3).toString();
-      });
-      this.EmployeeService.updateEmployees(this.updatedEmployee).subscribe();
-    } else {
-      return;
+    else if(this.updateClicked) {
+      if (employee.name.length < 2) {
+        this.nameError = true;
+      } else {
+        this.nameError = false;
+      }
+      if (employee.surname.length < 2) {
+        this.surnameError = true;
+      } else {
+        this.surnameError = false;
+      }
+      if (+employee.salary <= 0) {
+        this.salaryError = true;
+      } else {
+        this.salaryError = false;
+      }
+      if (!this.nameError && !this.surnameError && !this.salaryError) {
+        this.updatedEmployee.name = employee.name;
+        this.updatedEmployee.surname = employee.surname;
+        this.updatedEmployee.salary = employee.salary;
+        this.ConvertService.convertSalary(employee.salary.toString()).subscribe(response => {
+          this.updatedEmployee.salaryUsd = response.result.toFixed(3).toString();
+        });
+        this.EmployeeService.updateEmployees(this.updatedEmployee).subscribe();
+      } else {
+        return;
+      }
     }
   }
 
